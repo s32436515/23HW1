64a6b8875ca26c08613ea993cb39570f09e7028b
Cyrus_R <sandrew14556614@gmail.com>
2023-08-31 18:56:34 +0800

    switch to gameScene

--- 23HW1/Assets/Scripts/repeatRoad.cs
+++ 23HW1/Assets/Scripts/repeatRoad.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class repeatRoad : MonoBehaviour
 {
@@ -9,5 +10,10 @@ public class repeatRoad : MonoBehaviour
     {
         float roadX = GetComponent<Renderer>().material.GetTextureOffset("_MainTex").x;
         GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(roadX + speed * Time.deltaTime, 0));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
     }
 }
