f036efce8bd2f812135224aac00bc4eb22bb4ff8
Cyrus_R <sandrew14556614@gmail.com>
2023-09-04 21:19:12 +0800

    GameScene Completed!!
    removed leantween folder

--- 23HW1/Assets/Scripts/GameManager.cs
+++ 23HW1/Assets/Scripts/GameManager.cs
@@ -1,22 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject goat1, goat2, goat3, goat4, goat5;
+    [SerializeField] EnemySpawner enemySpawner;
+
+    void Awake()
     {
-        
+        goat1.SetActive(false);
+        goat2.SetActive(false);
+        goat3.SetActive(false);
+        goat4.SetActive(false);
+        goat5.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene("MenuScene");
+        }
+
+        switch (enemySpawner.levelNow)
+        {
+            case 1:
+                goat1.SetActive(true);
+                break;
+            case 2:
+                goat2.SetActive(true);
+                break;
+            case 3:
+                goat3.SetActive(true);
+                break;
+            case 4:
+                goat4.SetActive(true);
+                break;
+            case 5:
+                goat5.SetActive(true);
+                SceneManager.LoadScene("EndScene");
+                break;
         }
     }
 }
